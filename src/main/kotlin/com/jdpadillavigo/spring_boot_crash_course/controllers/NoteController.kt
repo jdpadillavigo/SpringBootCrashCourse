eab1b3e52f183a9dc489b37058d77567397672ea
Juan Diego Padilla <172089930+jdpadillavigo@users.noreply.github.com>
2026-03-30 21:40:21 -0500

    Saving notes to MongoDB

---
+++ src/main/kotlin/com/jdpadillavigo/spring_boot_crash_course/controllers/NoteController.kt
@@ -0,0 +1,72 @@
+package com.jdpadillavigo.spring_boot_crash_course.controllers
+
+import com.jdpadillavigo.spring_boot_crash_course.controllers.NoteController.NoteResponse
+import com.jdpadillavigo.spring_boot_crash_course.database.model.Note
+import com.jdpadillavigo.spring_boot_crash_course.database.repository.NoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+// POST http://localhost:8085/notes
+// GET http://localhost:8085/notes?ownerId=123
+
+@RestController
+@RequestMapping("/notes")
+class NoteController(
+    private val repository: NoteRepository
+) {
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val ownerId: String?
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant
+    )
+
+    @PostMapping
+    fun save(body: NoteRequest): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId(body.ownerId)
+            )
+        )
+
+        return note.toResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = true) ownerId: String
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.toResponse()
+        }
+    }
+}
+
+private fun Note.toResponse(): NoteResponse {
+    return NoteResponse(
+        id = id.toHexString(),
+        title = title,
+        content = content,
+        color = color,
+        createdAt = createdAt
+    )
+}
